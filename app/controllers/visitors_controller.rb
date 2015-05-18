66d80998be9f208bd4a24c0c27118349a0313d7f
kxhitiz <nirvanagrg@gmail.com>
2015-05-18 09:50:54 -0500

    Sort by correlation value.

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,7 +3,7 @@ class VisitorsController < ApplicationController
   def index
     # movie_id = params[:movie_id]
     if params[:movie_id].present?
-      @movies = Movie.where(movieId: params[:movie_id].to_i).limit(15)
+      @movies = Movie.where(movieId: params[:movie_id].to_i).asc(:regularizedCorRelation).limit(15)
       # @movies = Movie.limit(Movie::LIMIT).to_a.group_by(&:movieId)
       # @movies = movies.group_by(&:movieId).first(5)
     end
